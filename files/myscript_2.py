c9e72a6dffa40b596c8e6ae383593cc78063fe16
sabrsyed <sabriya.syed@gmail.com>
2017-10-17 11:58:17 -0400

    Python_01 script2

---
+++ files/myscript_2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import sys
+
+a= sys.argv[1] #name
+b= sys.argv[2] #favorite color
+c= sys.argv[3] #favorite activity
+d= sys.argv[4] #favorite animal
+
+print(a, b, c, d)
+
+
+ #print a,b,c,d inputed
+
+
+
+
