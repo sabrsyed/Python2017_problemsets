bdcef1d1cb08b328bff9ace5b7287eb6bfb2ad8c
sabrsyed <sabriya.syed@gmail.com>
2017-10-18 21:32:00 -0400

    script for Python04 question 2

---
+++ files/myscript_11.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+#Python_04 question 2
+
+
+
+number = 1000
+factorial_product = 1000
+
+while number < 1001 and number > 1: 
+	number-= 1
+	factorial_product *= number        
+
+print("number:" , number, "factorial_product:", factorial_product)
+print("All done!")
+
+
+	#print("number:" , number, "factorial_product:", factorial_product)
