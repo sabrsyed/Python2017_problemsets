64ed01019ed294d2d67e2535a12fbb341f62945d
sabrsyed <sabriya.syed@gmail.com>
2017-10-19 21:42:55 -0400

    script for Python05 question 3

---
+++ files/myscript_16.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+#Python_05 question 3
+
+
+fasta_read = open ("../Python_05.fasta", "r")
+fasta_write = open ("../Python_05_rc.fasta", "w")
+
+for dna in fasta_read:
+	
+	dna = dna.rstrip()
+	dna_reverse= dna.replace('T', 'a').replace('A', 't').replace('G', 'c').replace('C', 'g') 
+	fasta_write.write(dna_reverse[::-1])
+
+fasta_read.close()
+fasta_write.close()
+
+
