b7b794927dc52400f6f83a90aaa7ad39332d5feb
sabrsyed <sabriya.syed@gmail.com>
2017-10-24 17:18:14 -0400

    Python_06 question2

---
+++ files/myscript_22.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+#Python_06 question 1
+
+import re
+
+poem_read = open ("../Python_06_nobody.txt", "r")
+someone = open ("../someone.txt", "w")
+
+poem= poem_read.read()
+print(poem)
+
+newpoem = poem.replace('Nobody','Someone')
+someone.write(newpoem)
+
+poem_read.close()
+someone.close()
